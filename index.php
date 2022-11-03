f7781bc1130f9b8a5cd432ef3006d934f2d9a7b9
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-03 09:59:04 +0100

    link

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 echo '<p>First web on Azure</p>'; 
 echo '<h1>Nadpis urovne 1</h1>';
 echo '<h2>Nadpis urovne 2</h2>';
+echo '<a href="https://github.com/MatusKonopinsky/KonopinskyIoT">GIT</a>'
 
 
 ?>
