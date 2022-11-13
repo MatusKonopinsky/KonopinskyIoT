68235f00c5bdc5fcb4b9b93eb48912684326c1af
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-13 15:13:56 +0100

    form

--- webform.php
+++ webform.php
@@ -103,6 +103,7 @@ function test_input($data) {
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
   Name: <input type="text" name="name" value="<?php $_GET["name"];?>">
+  <br><br>
   <span class="error"><?php echo $nameErr;?></span>
   <br><br>
   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
