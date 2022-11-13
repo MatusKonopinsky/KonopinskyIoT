f3ca59d7d0f93c25e13cbfb0a35f85f42901884e
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-13 14:07:02 +0100

    form

--- webform.php
+++ webform.php
@@ -19,11 +19,28 @@ body, html {
     background-size: cover;
     filter: blur(8px);
 }
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+}
 </style>
 </head>
 <body>  
 
 <div class="bg-image"></div>
+
+<div class="bg-text">
 
 <?php
 // define variables and set to empty values
@@ -78,5 +95,6 @@ echo "<br>";
 echo $gender;
 ?>
 
+</div>
 </body>
 </html>
