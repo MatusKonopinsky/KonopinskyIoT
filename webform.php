8ad074aa17d2c507a097e84b6805dede8c394690
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-13 16:09:21 +0100

    form

--- webform.php
+++ webform.php
@@ -47,56 +47,6 @@ body, html {
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $age = $comment = $website = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
 <h2>Webform</h2>
@@ -129,6 +79,19 @@ function test_input($data) {
 </form>
 
 <?php
+$name = $email = $gender = $age = $comment = $website = "";
+
+$name = $_POST["name"];
+
+$email = $_POST["email"];
+
+$gender = $_POST["gender"];
+
+$age = $_POST["age"];
+
+$website = $_POST["website"];
+
+$comment = $_POST["comment"];
 
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['website']) && isset($_POST['comment']) && isset($_POST['gender'])){
 
@@ -152,6 +115,8 @@ echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
 echo $email;
+echo "<br>";
+echo $age;
 echo "<br>";
 echo $website;
 echo "<br>";
