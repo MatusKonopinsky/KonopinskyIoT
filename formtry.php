cf933dd66d873819018e1d378b81391b655f06af
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-13 15:58:36 +0100

    form

--- formtry.php
+++ formtry.php
@@ -15,6 +15,13 @@ $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
+    $name = test_input($_POST["name"]);
+
+    $email = test_input($_POST["email"]);
+
+    $website = test_input($_POST["website"]);
+
+    $comment = test_input($_POST["comment"]);
 
 }
 
