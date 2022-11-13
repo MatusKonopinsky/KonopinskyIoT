13d1cbcf741e22af0647d36a67a2e53dff969861
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-13 16:01:34 +0100

    form

--- formtry.php
+++ formtry.php
@@ -15,22 +15,16 @@ $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
-    $name = test_input($_POST["name"]);
+    $name = $_POST["name"];
 
-    $email = test_input($_POST["email"]);
+    $email = $_POST["email"];
 
-    $website = test_input($_POST["website"]);
+    $website = $_POST["website"];
 
-    $comment = test_input($_POST["comment"]);
+    $comment = $_POST["comment"];
 
 }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
 <h2>PHP Form Validation Example</h2>
