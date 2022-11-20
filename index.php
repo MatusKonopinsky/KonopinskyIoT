fa69a903919fdb2126dd5700169dfbdf4a368df4
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-11-20 20:04:11 +0100

    aaa

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS.css" type="text/css">
     <link rel="shortcut icon" href="image-bonfire-icon.png" type="image/icon type">
     <title>IoT Zadanie 2</title>
     <style>
