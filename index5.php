80f71a66726e6559ea6926151ed69339f11751a7
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-12-10 17:27:36 +0100

    index5

--- index5.php
+++ index5.php
@@ -14,6 +14,13 @@
 
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
+
+        $text = "a=" . $sn1 . " b=" . $sn2;
+        $sum = $sn1 + $sn2;
+        
+        echo $text;
+        echo "<br>";
+        echo "Sum:" . $sum;
         
         $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
         $text1 = "a=" . $sn1 . " b=" . $sn2;
