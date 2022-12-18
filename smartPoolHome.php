2c6a07f2a0b84110622daecdb53dfd248a004184
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-12-18 22:46:54 +0100

    update

--- smartPoolHome.php
+++ smartPoolHome.php
@@ -42,34 +42,28 @@
             if(isset($_GET['temperature'])){
 
                 $txt = fopen('sensorData.txt','w') or die("Unable to open file!");
-                
+
                 fwrite($txt, "Temperature: ".$_GET['temperature'].PHP_EOL);
                 fwrite($txt, "Part of day: ".$_GET['lightAmount'].PHP_EOL);
-                fwrite($txt, "Height of water: ".$_GET["height"].PHP_EOL);
+                fwrite($txt, "Height of water: ".$_GET['height'].PHP_EOL);
 
                 fclose($txt);
 
 
 
                 echo "<h2>Sensor data:</h2>";
-                echo "Temperature: ".$_GET["temperature"];
+                echo "Temperature: ".$_GET['temperature'];
                 echo "<br>";
-                echo "Part of day: ".$_GET["lightAmount"];
+                echo "Part of day: ".$_GET['lightAmount'];
                 echo "<br>";
-                echo "Height of water: ".$_GET["height"];
+                echo "Height of water: ".$_GET['height'];
                 echo "<br>";
             }
 
 
 
 
-            echo "<h2>Sensor data:</h2>";
-            echo "Temperature: ".$_GET["temperature"];
-            echo "<br>";
-            echo "Part of day: ".$_GET["lightAmount"];
-            echo "<br>";
-            echo "Height of water: ".$_GET["height"];
-            echo "<br>";
+            
         ?>
     </div>  
 
