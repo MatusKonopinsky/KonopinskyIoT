257e64086630363aebecf0dbd56bdc97fecddd07
MatusKonopinsky <114649886+MatusKonopinsky@users.noreply.github.com>
2022-12-18 15:16:19 +0100

    update

--- smartPool.php
+++ smartPool.php
@@ -13,7 +13,7 @@
 <body>
     <?php
         // define variables and set to empty values
-        $temperature = $lightAmount = $height = $valveDeg = $occupation = $permission = "";
+        $temperature = $lightAmount = $height = $valveDeg = "";
         
     ?>
     <div class="header">
